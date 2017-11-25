df8bd40118bb08670fba41086d654c93fdb22faf
EduardoSimon <al341513@uji.es>
2017-11-25 02:18:56 +0100

    Improved Level Editor Tool
    Added functionalities:
    -Generate Level in Editor
    -Clear Level in Editor
    -Save the level as a prefab

---
+++ Assets/Editor/LevelGeneratorEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    LevelGenerator script;
+    GameObject hierarchy;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    
+        script = target as LevelGenerator;
+
+        if (GUILayout.Button( "Generate"))
+        {
+            script.GenerateLevel();
+        }
+
+        if (GUILayout.Button("Clear Level"))
+        {
+            EditorSceneManager.OpenScene("Assets/_Scenes/NO DEFINITIVO/LevelGenerator.unity");
+        }
+
+        if (GUILayout.Button("Save As Prefab"))
+        {
+            hierarchy = GameObject.FindGameObjectWithTag("Environment");
+
+            if ((AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Levels/" + script.prefabName + ".prefab",typeof(object)) == null))
+            {
+                PrefabUtility.CreatePrefab("Assets/Prefabs/Levels/" + script.prefabName + ".prefab", hierarchy);
+            }
+            else
+            {
+                EditorApplication.Beep();
+                Debug.LogError("There's already a prefab with that name in the directory, please choose another name. ");
+            }
+        }
+    }
+
+}
--- Assets/Scripts/levelGenerator/LevelGenerator.cs
+++ Assets/Scripts/levelGenerator/LevelGenerator.cs
@@ -13,16 +13,21 @@ public class LevelGenerator : MonoBehaviour {
     }
 
     public Texture2D colorMap;
+    public String prefabName;
     public float ratio = 3;
     [SerializeField]List<EntitySpawnData> entitiesToSpawn = new List<EntitySpawnData>();
+    GameObject parent;
 
     void Start()
     {
         GenerateLevel();
     }
 
-    private void GenerateLevel()
+    public void GenerateLevel()
     {
+        //Create the parent Object
+        parent = new GameObject("environment"){ tag = "Environment"};
+
         for (int x = 0; x < colorMap.width; x++)
         {
             for (int y = 0; y < colorMap.height; y++)
@@ -37,17 +42,15 @@ public class LevelGenerator : MonoBehaviour {
         Color pixelColor = colorMap.GetPixel(x, y);
 
         //Discard the transparent pixels
-        if (pixelColor == Color.white)
+        if (pixelColor == Color.white || pixelColor.a == 0)
             return;
-
-        print(pixelColor);
 
         for (int i = 0; i < entitiesToSpawn.Count; i++)
         {
             if (pixelColor == entitiesToSpawn[i].pixelColor)
             {
                 Vector2 spawnPos = new Vector2(x/ratio, y/ratio);
-                Instantiate(entitiesToSpawn[i].prefabToSpawn, spawnPos, Quaternion.identity,transform);
+                Instantiate(entitiesToSpawn[i].prefabToSpawn, spawnPos, Quaternion.identity,parent.transform);
             }
         }
     }
