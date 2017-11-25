e9004488e5e473d21f78b77838c696de32393c14
RaulMartinezEspinosa <33634679+RaulMartinezEspinosa@users.noreply.github.com>
2017-11-25 19:12:04 +0100

    Instrucciones
    Incluido menú de instrucciones

---
+++ Assets/Scripts/camera/camaraInstrucciones.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraInstrucciones : MonoBehaviour {   
+    
+    void Update () {       
+        //Guardamos en una variable las posiciones en Y de LimiteSuperior y LimiteInferior
+        float limiteSuperior = GameObject.Find("LimiteSuperior").transform.position.y;
+        float limiteInferior = GameObject.Find("LimiteInferior").transform.position.y;
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Mientras la posición de la cámara en Y sea inferior a la de LimiteSuperior se puede mover hacia arriba 
+            if (transform.position.y < limiteSuperior) {
+                //Movemos la cámara 0.15 puntos hacia arriba
+                transform.Translate(new Vector3(0, 0.15f, 0));                
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Mientras la posición de la cámara en Y sea superior a la de LimiteInferior se puede mover hacia abajo
+            if (transform.position.y > limiteInferior) { 
+                //Movemos la cámara 0.15 puntos hacia abajo
+                transform.Translate(new Vector3(0, -0.15f, 0));
+            }
+        }
+    }
+}
