2d93ba9e0abc9239c0b6b92bcde19a5d970f1e17
Rickyy <lacajadericky@gmail.com>
2017-11-26 21:53:08 +0100

    generación procedural de fondos (ready para movimiento parallax)

--- Assets/InstantiateBackgrounds.cs
+++ Assets/InstantiateBackgrounds.cs
@@ -7,7 +7,12 @@ public class InstantiateBackgrounds : MonoBehaviour {
     private float sizeBackgroundFront = 34.15f;
 
     public GameObject frontBackground;
-    public GameObject unionObject;
+    public GameObject middleBackground;
+    public GameObject backBackground;
+
+    public GameObject unionFrontObject;
+    public GameObject unionMiddleObject;
+    public GameObject unionBackObject;
 
 
     public GameObject[] FrontBackgrounds;
@@ -15,14 +20,36 @@ public class InstantiateBackgrounds : MonoBehaviour {
     public GameObject[] BackBackgrounds;
 
 
+    private GameObject unionFrontObjectAux;
+    private GameObject unionMiddleObjectAux;
+    private GameObject unionBackObjectAux;
 
-	// Use this for initialization
-	void Start () {
+    private Transform[] children;
 
-        FrontBackgrounds = new GameObject[3];
-        setInitFrontBackgrounds();
+    public int numCopies = 3;
+    // Use this for initialization
+
+
+    private void Awake()
+    {
+        children = GetComponentsInChildren<Transform>();
+        
+    }
+
+    void Start () {
+
+        FrontBackgrounds = new GameObject[numCopies];
+        MiddleBackgrounds = new GameObject[numCopies];
+        BackBackgrounds = new GameObject[numCopies];
+
         
 
+        setInitBackgrounds();
+        
+        if (numCopies >= 3)
+        {
+            numCopies = 3;
+        }
 	}
 	
 	// Update is called once per frame
@@ -31,24 +58,77 @@ public class InstantiateBackgrounds : MonoBehaviour {
 	}
 
 
-    public void changeFrontBackgrounds()
+    public void changeBackgrounds(string background)
     {
+        if (background == "frontObject")
+        {
+            Debug.Log("we are on changeFrontBackgrounds()");
 
 
-        GameObject frontBackground = FrontBackgrounds[0];
-        Vector3 position = frontBackground.transform.position;
-        //trasladamos el de mas a la izquierda al tope de la derecha (position.x += 2*size
+            //trasladamos el de mas a la izquierda al tope de la derecha (position.x += 2*size
 
-        FrontBackgrounds[0].transform.position += new Vector3(2 * sizeBackgroundFront, 0, 0);
-        unionObject.transform.position += new Vector3(sizeBackgroundFront, 0, 0);
+            FrontBackgrounds[0].transform.position += new Vector3(3 * sizeBackgroundFront, 0, 0);
+            Debug.Log("lo movemos");
+            unionFrontObjectAux.transform.position += new Vector3(sizeBackgroundFront, 0, 0);
 
 
-        //hacemos las asignaciones necesarias
-        FrontBackgrounds[2] = FrontBackgrounds[0];
-        FrontBackgrounds[1] = FrontBackgrounds[2];
-        FrontBackgrounds[0] = FrontBackgrounds[1];
+            //hacemos las asignaciones necesarias
+            GameObject save0 = FrontBackgrounds[0];
+            GameObject save1 = FrontBackgrounds[1];
+            GameObject save2 = FrontBackgrounds[2];
 
-       
+            FrontBackgrounds[0] = save1;
+            FrontBackgrounds[1] = save2;
+            FrontBackgrounds[2] = save0;
+
+        }
+        
+        if (background == "middleObject")
+        {
+            Debug.Log("we are on changeFrontBackgrounds()");
+
+
+            //trasladamos el de mas a la izquierda al tope de la derecha (position.x += 2*size
+
+            MiddleBackgrounds[0].transform.position += new Vector3(3 * sizeBackgroundFront, 0, 0);
+            unionMiddleObjectAux.transform.position += new Vector3(sizeBackgroundFront, 0, 0);
+
+
+            //hacemos las asignaciones necesarias
+            GameObject save0 = MiddleBackgrounds[0];
+            GameObject save1 = MiddleBackgrounds[1];
+            GameObject save2 = MiddleBackgrounds[2];
+
+            MiddleBackgrounds[0] = save1;
+            MiddleBackgrounds[1] = save2;
+            MiddleBackgrounds[2] = save0;
+
+
+        }
+        if (background == "backObject")
+        {
+            Debug.Log("we are on changeFrontBackgrounds()");
+
+
+            //trasladamos el de mas a la izquierda al tope de la derecha (position.x += 2*size
+
+            BackBackgrounds[0].transform.position += new Vector3(3 * sizeBackgroundFront, 0, 0);
+            unionBackObjectAux.transform.position += new Vector3(sizeBackgroundFront, 0, 0);
+
+
+            //hacemos las asignaciones necesarias
+            GameObject save0 = BackBackgrounds[0];
+            GameObject save1 = BackBackgrounds[1];
+            GameObject save2 = BackBackgrounds[2];
+
+            BackBackgrounds[0] = save1;
+            BackBackgrounds[1] = save2;
+            BackBackgrounds[2] = save0;
+
+        }
+
+        
+
         
 
 
@@ -57,17 +137,29 @@ public class InstantiateBackgrounds : MonoBehaviour {
 
     }
 
-    public void setInitFrontBackgrounds()
+    public void setInitBackgrounds()
     {
+        
 
-        for (int i = 0; i < FrontBackgrounds.Length;i++)
+        for (int i = 0; i < numCopies;i++)
         {
-            FrontBackgrounds[i] = frontBackground;
+            /*
+            GameObject aux = frontBackground;
             //lo instanciamos uno seguido de otro
-            Instantiate(FrontBackgrounds[i], new Vector3(20 + i * sizeBackgroundFront, 0, 0), FrontBackgrounds[0].transform.rotation);
+            Instantiate(aux, new Vector3(20 + i * sizeBackgroundFront, -3, 0), aux.transform.rotation);
+            FrontBackgrounds[i] = aux;
+            */
+            FrontBackgrounds[i] = Instantiate(frontBackground, new Vector3(20 + i * sizeBackgroundFront, -4, 0), frontBackground.transform.rotation, children[1]);
+            MiddleBackgrounds[i] = Instantiate(middleBackground, new Vector3(30 + i * sizeBackgroundFront, -3, 0), middleBackground.transform.rotation, children[2]);
+            BackBackgrounds[i] = Instantiate(backBackground, new Vector3(40 + i * sizeBackgroundFront, -2, 0), backBackground.transform.rotation, children[3]);
 
         }
-        Instantiate(unionObject, new Vector3(FrontBackgrounds[1].transform.position.x + sizeBackgroundFront / 2, 0,0), unionObject.transform.rotation);
+        
+
+        unionFrontObjectAux = Instantiate(unionFrontObject, new Vector3(FrontBackgrounds[1].transform.position.x + sizeBackgroundFront / 2, -3,0), unionFrontObject.transform.rotation, children[1]);
+        unionMiddleObjectAux = Instantiate(unionMiddleObject, new Vector3(MiddleBackgrounds[1].transform.position.x + sizeBackgroundFront / 2, -3, 0), unionMiddleObject.transform.rotation, children[2]);
+        unionBackObjectAux = Instantiate(unionBackObject, new Vector3(BackBackgrounds[1].transform.position.x + sizeBackgroundFront / 2, -3, 0), unionBackObject.transform.rotation, children[3]);
+
 
     }
 }
