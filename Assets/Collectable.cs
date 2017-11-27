eaec1d52805575fcacb78ccbadd779008b7ceed9
Eduardo Simón Picón <al341513@uji.es>
2017-11-27 21:16:10 +0100

    Added collectable movement

---
+++ Assets/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public float amplitude = 5.0f;
+
+    private float startingY;
+
+    void Start()
+    {
+        startingY = transform.position.y;
+    }
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    float newYPosition = startingY + (Mathf.Sin(Time.time)) * amplitude;
+        this.transform.position = new Vector3(transform.position.x,newYPosition,transform.position.z);
+	}
+}
