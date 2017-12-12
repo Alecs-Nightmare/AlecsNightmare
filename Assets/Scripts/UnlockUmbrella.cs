b955c1bd50c10e34a31ee303ec77bed8af4d4119
Eduardo Simón Picón <al341513@uji.es>
2017-12-12 17:31:23 +0100

    Some Fixes paraguas prefab

--- Assets/Scripts/UnlockUmbrella.cs
+++ Assets/Scripts/UnlockUmbrella.cs
@@ -4,34 +4,12 @@ using UnityEngine;
 
 public class UnlockUmbrella : MonoBehaviour {
 
-    //public GameObject _playerMovement;
-
-    // Set up references
-    /*
-    void Awake()
-    {
-        _playerMovement = GameObject.FindGameObjectWithTag("PlayerMovement");
-    }
-    */
-
-    // Use this for initialization
-    void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnTriggerEnter2D(Collider2D col)
+    public void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
         {
-            
             col.GetComponent<PlayerMovement>().UmbrellaUnlocked = true;
-
+            Destroy(this);
         }
     }
 }
