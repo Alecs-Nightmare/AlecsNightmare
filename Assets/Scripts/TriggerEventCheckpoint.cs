4d7431022f39047b3e010294ce11e7c041bc2774
mariot11 <mariot3r@gmail.com>
2017-12-12 01:20:00 +0100

    checkpoint

---
+++ Assets/Scripts/TriggerEventCheckpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEventCheckpoint : MonoBehaviour {
+
+    private Animator anim;
+    private string activado = "activado"; 
+
+    private void Awake()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            anim.SetTrigger(activado);
+        }
+    }
+}
