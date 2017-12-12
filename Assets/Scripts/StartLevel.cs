9df1dfcd7723617a07700509d68a33793a6bddc9
Eduardo Simón Picón <al341513@uji.es>
2017-12-12 17:21:00 +0100

    Organized Scenes and Platform Fix

---
+++ Assets/Scripts/StartLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel : MonoBehaviour
+{
+    public void ButtonClicked()
+    {
+        GameManager.instance.LoadSpecificScene("Scene1");
+    }
+    
+}
