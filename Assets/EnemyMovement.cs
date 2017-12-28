eb0f76c168bd14024c2226c957e95a047a648c89
Rickyy <lacajadericky@gmail.com>
2017-12-28 19:22:28 +0100

    pruebas IA

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public bool followPlayer;
+    public Transform target;
+    public float enemySpeed;
+    private Vector3 spawnPosition;
+    private string whereIsThePlayer;
+    private float localScaleX;
+    private float invLocalScaleX;
+    private GameObject[] bones;
+    private GlassweatherAnimationController glAnimContr;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        glAnimContr = GetComponent<GlassweatherAnimationController>();
+        spawnPosition = this.transform.position;
+        
+
+    }
+    void Start () {
+
+        localScaleX = this.transform.localScale.x;
+        invLocalScaleX = localScaleX * -1;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        whereIsThePlayer = target.position.x >= transform.position.x ? "right" : "left";
+        ManageEnemyMovement();
+       
+	}
+
+    public void ManageEnemyMovement()
+    {
+        if (followPlayer)
+        {
+            glAnimContr.alertingPlayer = true;
+            glAnimContr.ignoringPlayer = false;
+            print("follow player");
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(target.position.x, this.transform.position.y, 0), enemySpeed * Time.deltaTime);
+            if (whereIsThePlayer == "right")
+                this.transform.localScale = new Vector3(localScaleX, this.transform.localScale.y, this.transform.localScale.z);
+            else
+                this.transform.localScale = new Vector3(invLocalScaleX, this.transform.localScale.y, this.transform.localScale.z);
+
+        }
+        else
+        {
+            glAnimContr.ignoringPlayer = true;
+            glAnimContr.followingPlayer = false;
+            glAnimContr.alertingPlayer = false;
+            this.transform.position = Vector3.MoveTowards(this.transform.position, spawnPosition, (enemySpeed - 1) * Time.deltaTime);
+            if (whereIsThePlayer == "right")
+                this.transform.localScale = new Vector3(invLocalScaleX, this.transform.localScale.y, this.transform.localScale.z);
+            else
+                this.transform.localScale = new Vector3(localScaleX, this.transform.localScale.y, this.transform.localScale.z);
+        }
+    }
+
+    public float DistanceToPlayer()
+    {
+        return Vector2.Distance(target.position, this.transform.position);
+    }
+}
---
+++ Assets/GlassweatherAnimationController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassweatherAnimationController : MonoBehaviour {
+    private Animator anim;
+    private EnemyMovement _enemyMovement;
+
+    public bool followingPlayer;
+    public bool ignoringPlayer;
+    public bool dying;
+    public bool alertingPlayer;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        _enemyMovement = GetComponent<EnemyMovement>();
+    }
+
+    void Start() {
+
+
+    }
+
+    enum EnemyState
+    {
+        idle = 0,
+        alerting = 1,
+        following = 2,
+        dying = 3
+    }
+    // Update is called once per frame
+    void Update() {
+
+        if (followingPlayer)
+        {
+            anim.SetInteger(AnimatorParameters.estado, (int)EnemyState.following);
+
+        }
+        else if (alertingPlayer)
+        {
+            anim.SetInteger(AnimatorParameters.estado, (int)EnemyState.alerting);
+            followingPlayer = true;
+        }
+        else if (dying)
+        {
+            anim.SetInteger(AnimatorParameters.estado, (int)EnemyState.dying);
+        }
+        else if (ignoringPlayer)
+        {
+            anim.SetInteger(AnimatorParameters.estado, (int)EnemyState.idle);
+        }
+
+    }
+    [System.Serializable]
+    public class AnimatorParameters
+    {
+        public static string estado = "estado";
+    }
+
+}
