eb1dc7e7123e0dd859cd03020d2f0412d85c4ea1
Ricardo <lacajadericky@blogspot.com>
2018-01-21 02:15:26 +0100

    boss in progress

---
+++ Assets/DamageCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCounter : MonoBehaviour {
+    private bool playerInside;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(playerInside);
+	}
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            playerInside = true;
+
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            playerInside = true;
+
+        }
+    }
+}
---
+++ Assets/Destroy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+    private float fallingSpeed;
+    public float minFallingSpeed;
+    public float maxFallingSpeed;
+	// Use this for initialization
+	void Start () {
+        fallingSpeed = Random.Range(minFallingSpeed, maxFallingSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+	}
+
+    public void Move()
+    {
+
+        transform.Translate(Vector3.down * fallingSpeed * Time.deltaTime);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            print("Nos ha golpeado un proyectil!");
+
+        }
+        else if (collider.gameObject.CompareTag("AWall") || collider.gameObject.CompareTag("Through"))
+        {
+
+            print("LOL");
+            Destroy(this.gameObject);
+
+        }
+    }
+}
